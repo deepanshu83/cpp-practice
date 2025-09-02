cfbed0e9badc55122f28a4a1a13642818d8b7a86
deepanshu83 <dd893566039@gmail.com>
2025-09-02 13:41:47 +0530

    new commit

---
+++ 7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // variable name declare karne ke rules 
+   
+    // variable name start 'alphabet' and "underscore '_' " and '$' in teeno chizoo se hota h 
+    // special reserved aprox 32 keywords h unnse nhi declare kr sakte 
+    // commas and blank space is not allowed 
+
+    int _h = 9 ;
+
+    cout<< _h << endl;
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # cpp-practice
+
+<!-- copmile and run aase karo 
+
+jaise mere file ka naam 6.cpp h to 
+
+g++ 6.cpp -o 6 && ./6
+
+ye -o h wo output file banata h 6 ki jagha or naam bhi de sakate h pr file ka naam de de to shi h 
+
+ -->
+
+ 
