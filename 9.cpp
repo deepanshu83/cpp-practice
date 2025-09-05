37717c817c30436f23a0e3daa55a3bcd7c996219
deepanshu83 <dd893566039@gmail.com>
2025-09-05 19:37:54 +0530

    new commit

---
+++ 9.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int number ;
+
+    cout<<"enter your number "<<endl;
+    cin>>number;
+
+    if(number/2==0){
+        cout<<"Your entered number is even "<<endl;
+    }
+    else{
+        cout<<"Your entered number is odd "<<endl;
+    }
+
+
+    return 0;
+}
