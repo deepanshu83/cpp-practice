7858c0237ba1458d810f3c6c819974de67b2a22e
deepanshu83 <dd893566039@gmail.com>
2025-09-05 20:11:54 +0530

    commit

---
+++ 11.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+int main(){
+
+    double n;
+
+    cout<<"Enter a integer number to get a absolute value "<<endl;
+    cin>>n;
+
+    cout<<"New value is "<< round(abs(n)) <<endl;
+
+    return 0;
+}
